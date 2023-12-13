ce8957d7d95ca8609004b85ecac7406e25f5d947
programadoragain <ferdandi04@gmail.com>
2023-12-13 15:27:55 -0300

    gg

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { TokenInterceptorInterceptor } from './app/security/token-interceptor.interceptor';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule), provideRouter(routes), provideAnimations()],
+  providers: [
+              importProvidersFrom(HttpClientModule),
+              provideRouter(routes), 
+              provideAnimations(),
+              { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorInterceptor, multi: true }
+            ],
 }).catch((err) => console.error(err));
